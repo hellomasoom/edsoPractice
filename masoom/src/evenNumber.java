14219b97c7ea8c35bcfbfe6592e1306b2e27f3f6
MD Masoom Reza <hellomasoom789@gmail.com>
2025-08-24 16:34:33 +0530

    done

--- masoom/src/evenNumber.java
+++ masoom/src/evenNumber.java
@@ -1,10 +1,11 @@
 public class evenNumber {
     public static void main(String[] args) {
+int j =100;
         System.out.println("Even numbers from 1 to 100 are:");
 
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
-                System.out.print(i + " ");
+               
             }
         }
     }
